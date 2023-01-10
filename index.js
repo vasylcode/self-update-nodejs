ecf97b6ce2b8a8933306dd03ae403e954ea9188f
vasylcode <vasylcode@gmail.com>
2023-01-10 13:58:28 +0100

    version route

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const packageJson = require('./package.json');
 
-app.get('/', (req, res) => {
+app.get('/version', (req, res) => {
 	res.set('Content-Type', 'text/plain');
-	res.send('Hello, World!');
+	res.send(packageJson.version);
 });
 
 app.listen(3000);
