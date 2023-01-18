d9a1d63ca72c7efae4d200236563d32d2044de01
vasylcode <vasylcode@gmail.com>
2023-01-18 17:03:04 +0100

    script update

--- update.sh
+++ update.sh
@@ -7,18 +7,15 @@ fi
 
 # Store the current version of the app
 current_version=$(node -e "console.log(require('./package.json').version)")
-echo "Current version = $current_version"
-
 # Fetch the latest version from your domain
-latest_version=$(curl -s "http://localhost:3000/version")
-echo "Latest version = $latest_version"
+latest_version=$(curl -s "http://yourdomain.com/version")
+echo "[$(date)] Current v$current_version, latest - v$latest_version"
 
 # Check if the latest version is newer than the current version
 if [ "$latest_version" != "$current_version" ]; then
     # Update the app from Github
     echo "[$(date)] New version $latest_version is detected! Old version: $current_version. Updating..."
-    git pull origin master
+    git pull
     npm install
     pm2 restart index.js
-
 fi
